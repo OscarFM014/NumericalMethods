7ea42be7c3239077cd4e09269688bdd0e507f570
Oscar Fernandez <oscar.gohan2010@hotmail.com>
2020-09-21 22:32:02 -0500

    Gauss Seidel

--- ProgActivities/Homework/4.Gauss-Seidel.py
+++ ProgActivities/Homework/4.Gauss-Seidel.py
@@ -5,25 +5,21 @@ def gauss_seidel(a, b, n, xInv, iMax, tol, relFac):
     x = []
     x = xInv
     for i in range(n):
-        dummy = a[i][i]
+        dummy = a[i][i] + 0.0
         if dummy > tol:
             for j in range(n):
-                a[i][j] = a[i][j] * dummy
+                a[i][j] = a[i][j] / dummy
             b[i] = b[i]/dummy
-
         else:
             return "Error hay un cero en la diagonal"
-
     for i in range(n):
         suma = b[i]
         for j in range(n):
             if i != j:
                 suma = suma - (a[i][j]*x[j])
-        xI = suma
-
-    itera = 1
+        x[i] = suma
+    itera = 0
     sentinel = True
-
     while sentinel == True and (itera < iMax):
         sentinel = False
         for i in range(n):
@@ -35,26 +31,14 @@ def gauss_seidel(a, b, n, xInv, iMax, tol, relFac):
             x[i] = relFac * suma+(1-relFac)*old
             if (sentinel == False and math.fabs(x[i]) > tol):
                 ea = math.fabs((x[i] - old)/x[i])
-                # ed no existe WFT
+                # ed no existe lo cambiamos por la tolerancia
                 if ea > tol:
                     sentinel = True
         itera += 1
     return x
 
 
-if __name__ == "__main__":
-     a = [[1, 1, 1, 0, 0, 0],
-         [0, -1, 0, 1, -1, 0],
-         [0, 0, -1, 0, 0, 1],
-         [0, 0, 0, 0, 1, -1],
-         [0, 10, -10, 0, -15, -5],
-         [5, -10, 0, -20, 0, 0]]
-    b = [0,0,0,0,0,200]
-    tol = 0.5
-    n = 6 
-    iMax = 0
-    relFac = 0
-    xInv = []
-
-    # Linda Nayeli
-    # Oscar Fernández 
+"""
+Linda Nayeli
+Oscar Fernandez
+"""
